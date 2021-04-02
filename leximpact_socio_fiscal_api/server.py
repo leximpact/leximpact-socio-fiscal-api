c3682f7e855a7c47a147dc1132f25a705e91c0d6
Emmanuel Raviart <emmanuel@raviart.com>
2021-04-02 17:04:38 +0200

    Remove home page.

--- leximpact_socio_fiscal_api/server.py
+++ leximpact_socio_fiscal_api/server.py
@@ -1,45 +1,11 @@
 import asyncio
+import numpy as np
 
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
 from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_france import FranceTaxBenefitSystem
 
 app = FastAPI()
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
 tax_benefit_system = FranceTaxBenefitSystem()
 
@@ -52,11 +18,6 @@ def walDecompositionLeafs(node):
         # Note: Ignore nodes that are the total of their children.
     else:
         yield node
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
 
 
 @app.websocket("/ws")
